de2ad720832b2fa992eaa520ab598dc17c60acbd
hillols <45110654+hillols@users.noreply.github.com>
2020-01-25 15:05:17 -0500

    Add files via upload

---
+++ Assignment 3 - First Github Repo.py
@@ -0,0 +1,16 @@
+# This is the code for calculating area and perimeter of a circle.
+# It will be the first submission to Github repo.
+
+pi=3.14 # value of pi assigned upto two decimal points.
+r=2 # value of radius assigned.
+area=(pi*r**2) # formula for calculation of area assigned.
+perim=(2*pi*r) #formula for calculation of perimeter assigned.
+
+#Print command for the required variables
+
+print ("Given r:", r)
+print ("Area is:", area)
+print ("Perimeter is:", perim)
+
+
+ 
